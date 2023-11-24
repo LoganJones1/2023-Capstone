305f5426fb7ac858148a8d7c1536edf92fb1ae74
nourjaffan <73398943+nourjaffan@users.noreply.github.com>
2023-11-24 16:24:09 -0700

    Update ros_communication.h

--- v7a Sept 2023/catkin_ws/src/mapping/src/ros_communication.h
+++ v7a Sept 2023/catkin_ws/src/mapping/src/ros_communication.h
@@ -2,9 +2,22 @@
 #define ROS_COMMUNICATION_H
 
 #include "ros/ros.h"
-#include <memory>
+#include <octomap/OcTree.h>
+#include <sensor_msgs/PointCloud2.h>
 
-class OctomapConverter;
+class OctomapConverter {
+public:
+    OctomapConverter();
+    ~OctomapConverter();
+
+private:
+    void pointCloudCallback(const sensor_msgs::PointCloud2& cloudMsg);
+
+    ros::NodeHandle n;
+    ros::Subscriber sub;
+    ros::Publisher pub;
+    octomap::OcTree* octree;
+};
 
 void setupRosCommunication(ros::NodeHandle& nh);
 
