4b03c2f0135c8f4aa11993c8c06e4ec0df3c4ea9
Ryan Huynh <73033285+R41Ryan@users.noreply.github.com>
2023-11-23 23:33:58 -0700

    octomap processing: Able to add PointCloud2 to OcTree

--- v7a Sept 2023/catkin_ws/src/mapping/src/octomap_processing.h
+++ v7a Sept 2023/catkin_ws/src/mapping/src/octomap_processing.h
@@ -4,7 +4,24 @@
 #include <octomap/octomap.h>
 #include <octomap/OcTree.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/Imu.h>
+#include <ros/ros.h>
 
+using namespace ros;
+
+// Class for keeping track of the inert frame of reference of the robot
+class Localization {
+   NodeHandle* nh_;
+   Subscriber sub;
+   Publisher pub;
+
+public:
+   Localization(NodeHandle* nh) : nh_(nh);
+   // When getting a message from the imu topic, this code will run.
+   imuCallback(const sensor_msgs::Imu);
+}
+
+// Process the PointCloud2 data into a format compatible with Octomap and adds to OcTree provided by the user.
 void processPointCloudToOctomap(const sensor_msgs::PointCloud2& cloudMsg, octomap::OcTree* octree);
 
 #endif
