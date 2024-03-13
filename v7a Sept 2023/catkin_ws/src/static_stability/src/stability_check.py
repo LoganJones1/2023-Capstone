3516fe8551d2498168f3e9fa2604915ebe87dee9
LoganJones1 <logan.jones1@ucalgary.ca>
2024-03-13 16:23:27 -0600

    integrated nours code into stability_check.py

--- v7a Sept 2023/catkin_ws/src/static_stability/src/stability_check.py
+++ v7a Sept 2023/catkin_ws/src/static_stability/src/stability_check.py
@@ -1,88 +1,184 @@
 """
 File: stability_check.py
 Author: Logan Jones
-Date: February 26, 2024
+Date: March 13, 2024
 
 Description: To check if the robot is stable in a static possition and send 
 a message on ros wether the robot is stable or not.
 """
 
+
 # Imports
-# Add import statements here if needed
+import rospy
+from std_msgs.msg import Bool
+from sensor_msgs.msg import Imu
+from sensor_msgs.msg import ForceTorqueSensorMessageType # Toshi, update this
+from geometry_msgs.msg import Vector3
+from dynamixel_sdk import *  # Assumes Dynamixel SDK library is correctly set up and accessible
+import tf.transformations
+from dynamixel_sdk import PortHandler, PacketHandler
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 # Constants
-# Add any global constants here
+IMU_TOPIC = "imu/data"
+FORCE_TORQUE_TOPIC = "" # Toshi, update this
+STABILITY_STATUS_TOPIC = "robot/stability_status"
+DYNAMIXEL_MOTOR_IDS = [1, 2, 3, 4]  #update
+DEVICENAME = '/dev/ttyUSB0'
+BAUDRATE = 57600
+PROTOCOL_VERSION = 2.0
+ADDR_PRESENT_POSITION = 123  #update
+
+
+# Global Variables
+vecGravity = Vector3()
+ftData = {}
+
+
+# Setup
+portHandler = PortHandler(DEVICENAME)
+packetHandler = PacketHandler(PROTOCOL_VERSION)
+if not portHandler.openPort():
+    rospy.logerr("Failed to open the port")
+    exit()
+if not portHandler.setBaudRate(BAUDRATE):
+    rospy.logerr("Failed to set the baud rate")
+    exit()
+
 
 # Functions
-# Define your functions here
+"""
+LOAD ENDPOINT VECTORS (Incomplete)
+ASSIGNEE: Logan Jones
+---------------------
+Subscribe to the ros topic that contains vectors from the center of mass
+to the end of each limb (Left hand and foot, right hand and foot).
+"""
+def load_endpoint_vectors():
+    # NEED TO BE DONE WHEN KINIMATICS ARE COMPLETED:
+    # 1) Subscribe to the kinimatics topic to recieve 4 vectors that represent the
+    # locations of the end of each limb and originate form the robot's center of
+    # mass.
+    # 2) Load those vectors into the following dictionary in place of the existing
+    # mock data.
+    endPointVecs = {'leftHand': Vector3(0.0, 2.0, 1.0),
+                    'rightHand': Vector3(-1.0,-2.0, 1.0),
+                    'leftFoot': Vector3(1.5, 0.5,-3.0),
+                    'rightFoot': Vector3(-0.5,-1.0,-3.0)}
+    return endPointVecs
+# End of: LOAD ENDPOINT VECTORS
+
+
+"""
+LOAD FORCE-TORQUE SENSOR DATA
+ASSIGNEE: Toshi Baswas
+-----------------------------
+Subscribe to the ros topic that contains values from the force-torque
+sensors and calculate wether that limb is in contact for stability.
+"""
+def force_torque_callback(data):
+    ftData = data
+
+def load_ft_data():
+    global ftData
+    # ADD CODE HERE
+    limbsInContact = {'leftHand': False, 'rightHand': False,    # mock data
+                      'leftFoot': False, 'rightFoot': False}    # needs updating
+    return limbsInContact
+# End of: LOAD FORCE-TORQUE SENSOR DATA
+
+
+"""
+UPDATE GRAVITY VECTOR
+ASSIGNEE: Logan Jones
+---------------------
+Load information from the IMU to find the direction of gravity. Create a
+vector to represent the direction of gravity from the center of mass of
+the robot.
+"""
+def imu_callback(data):
+    global vecGravity
+    # 1) subscribe to imu topic
+    # 2) extract direction of gravity from topic
+    vecGravity = Vector3(0.0, 0.0, 0.0) # mock data
+# End of: UPDATE GRAVITY VECTOR
+
+
+"""
+CALCULATE CONTACT POINTS
+ASSIGNEE: Aryan Taneja
+------------------------
+Calculate the points of the robot that are in contact for stability. The 
+contact points should be vectors that originate from the center of mass.
+"""
+def calc_contact_points(endPointVecs, limbsInContact):
+    # ADD CODE HERE
+    contactPoints = np.array([])  # an array/list of vectors
+    return contactPoints
+# End of: CALCULATE CONTACT POINTS
+
+
+"""
+CALCULATE POLYGON
+ASSIGNEE: Aryan Taneja
+----------------------
+Calculate the stabilty poligon that is the largest possible triangle from
+considering all contact points.
+"""
+def calc_polygon(contactPoints):
+    # ADD CODE HERE
+    cws = np.array([])    # List of the three contact points that make up the support poligon
+    return cws
+# End of: CALCULATE POLYGON
+
+
+"""
+CALCULATE STABILITY
+ASSIGNEE: Francis Nacu
+----------------------
+Calculate whether the center of mass (gravity vector) itersects with the
+support polygon.
+"""
+def is_stable(cws):
+    # should be replaced with something more thorough
+    polygon_points = np.array(cws)[:, :2]  # extract x y coordinates
+    gravity_point = vecGravity[:2]  # Ignoring Z coordinate
+    stable = plt.path.Path(polygon_points).contains_point(gravity_point)    # boolean
+    return stable
+# End of: CALCULATE STABILITY
+
 
 # Main code
 if __name__ == "__main__":
-    
-    """
-    LOAD ENDPOINT VECTORS
-    ---------------------
-    Subscribe to the ros topic that contains vectors from the center of mass
-    to the end of each limb (Left hand and foot, right hand and foot).
-    """
-    vecLeftHand = []    # a vector
-    vecRightHand = []   # a vector
-    vecLeftFoot = []    # a vector
-    vecRightFoot = []   # a vector
-    # End of: LOAD ENDPOINT VECTORS
+    # establish ros node
+    rospy.init_node('static_stability_check')
 
-    """
-    LOAD FORCE-TORQUE SENSOR DATA
-    -----------------------------
-    Subscribe to the ros topic that contains values from the force-torque
-    sensors and calculate wether that limb is in contact for stability.
-    """
-    contactLeftHand = 0     # a boolean
-    contactRightHand = 0    # a boolean
-    contactLeftFoot = 0     # a boolean
-    contactRightFoot = 0    # a boolean
-    # End of: LOAD FORCE-TORQUE SENSOR DATA
+    # setup sub and pub topics
+    rospy.Subscriber(IMU_TOPIC, Imu, imu_callback)
+    rospy.Subscriber(FORCE_TORQUE_TOPIC, ForceTorqueSensorMessageType, force_torque_callback)
+    stability_pub = rospy.Publisher(STABILITY_STATUS_TOPIC, Bool, queue_size=10)
 
-    """
-    LOAD THE DIRECTION OF GRAVITY
-    -----------------------------
-    Load information from the IMU to find the direction of gravity. Create a
-    vector to represent the direction of gravity from the center of mass of
-    the robot.
-    """
-    vecGravity = []   # a vector
-    # End of: CALCULATE CONTACT POINTS
+    # loop functionality
+    rate = rospy.Rate(10)  # 10 Hz
+    while not rospy.is_shutdown():
 
-    """
-    CALCULATE CONTACT POINTS
-    ------------------------
-    Calculate the points of the robot that are in contact for stability. The 
-    contact points should be vectors that originate from the center of mass.
-    """
-    contactPoints = []  # an array/list of vectors
-    # End of: CALCULATE CONTACT POINTS
+        # load data from topics/sensors
+        endPontVecs = load_endpoint_vectors()
+        limbsInContact = load_ft_data()
 
-    """
-    CALCULATE POLYGON
-    -----------------
-    Calculate the stabilty poligon that is the largest possible triangle from
-    considering all contact points.
-    """
-    cws = []    # List of all contact points that make up the support poligon
-    # End of: CALCULATE POLYGON
+        # calculate stability
+        contactPoints = calc_contact_points(endPontVecs, limbsInContact)
+        cws = calc_polygon(contactPoints)
+        stable = is_stable(cws)
 
-    """
-    CALCULATE STABILITY
-    -------------------
-    Calculate whether the center of mass (gravity vector) itersects with the
-    support polygon.
-    """
-    stable = False    # boolean
-    # End of: CALCULATE STABILITY
+        # publish stability
+        # Toshi, this covers one of your responsibilities
+        stability_pub.publish(Bool(stable))
 
-    """
-    PUBLISH STABILITY STATUS
-    ------------------------
-    Publish the status of the stability of the robot to the ROS system.
-    """
-    # End of: CALCULATE STABILITY
+        # sleep
+        rate.sleep()
+
+    portHandler.closePort()
+    # end of file
